f3c0100d25d9d3d4dd4d703440aff48e1607a626
Prachi290223 <prachi.raut@arguoft.in>
2025-08-06 10:58:07 +0530

    created form.py file

--- books/forms.py
+++ books/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import *
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Books
         fields = '__all__'
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model =Review
+        fields =['comment','rating']
+
+
